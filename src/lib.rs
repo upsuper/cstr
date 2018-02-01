64016f4bc07389e1957a3370c5a9fda094a556bd
Xidorn Quan <me@upsuper.org>
2018-02-02 09:52:18 +1100

    Add doc for the macro and move the test to example in doc.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,23 @@ pub use cstr_macros::*;
 
 define_invoke_proc_macro!(cstr__invoke_build_bytes);
 
+/// Expands to a expression typed `&'static CStr` with the given characters.
+/// (It currently only supports a UTF-8 string as input.)
+///
+/// Note that the expanded expression cannot be used to initialize `const`
+/// currently.
+///
+/// # Example
+///
+/// ```
+/// #[macro_use] extern crate cstr;
+/// use std::ffi::CStr;
+///
+/// # fn main() {
+/// let test = cstr!("hello");
+/// assert_eq!(test, CStr::from_bytes_with_nul(b"hello\0").unwrap());
+/// # }
+/// ```
 #[macro_export]
 macro_rules! cstr {
     ($t: tt) => {
@@ -19,16 +36,5 @@ macro_rules! cstr {
                 ::std::ffi::CStr::from_bytes_with_nul_unchecked(BYTES)
             }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::ffi::CStr;
-
-    #[test]
-    fn test_basic() {
-        let test: &'static CStr = cstr!("aaa");
-        assert_eq!(test, CStr::from_bytes_with_nul(b"aaa\0").unwrap());
     }
 }
