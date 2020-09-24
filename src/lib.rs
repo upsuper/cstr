7048d5b7f733710a47e48b978eae11778ad5af78
Xidorn Quan <me@upsuper.org>
2020-09-24 22:30:33 +1000

    Remove dependency to syn
    Closes #6.

--- src/lib.rs
+++ src/lib.rs
@@ -19,12 +19,15 @@
 //! assert_eq!(test, CStr::from_bytes_with_nul(b"hello\0").unwrap());
 //! ```
 
+use crate::parse::parse_input;
 use proc_macro::TokenStream as RawTokenStream;
 use proc_macro2::{Literal, Span, TokenStream};
-use quote::quote;
+use quote::{quote, quote_spanned};
 use std::ffi::CString;
-use syn::parse::{Parse, ParseBuffer};
-use syn::{Error, Ident, LitByteStr, LitStr, Result};
+
+mod parse;
+
+struct Error(Span, &'static str);
 
 #[proc_macro]
 pub fn cstr(input: RawTokenStream) -> RawTokenStream {
@@ -34,39 +37,19 @@ pub fn cstr(input: RawTokenStream) -> RawTokenStream {
                 #s as *const [u8] as *const ::std::ffi::CStr
             )
         }),
-        Err(e) => e.to_compile_error(),
+        Err(Error(span, msg)) => quote_spanned!(span => compile_error!(#msg)),
     };
     tokens.into()
 }
 
-fn build_byte_str(input: TokenStream) -> Result<Literal> {
-    let Input(bytes, span) = syn::parse2::<Input>(input)?;
-    CString::new(bytes)
-        .map(|s| {
+fn build_byte_str(input: TokenStream) -> Result<Literal, Error> {
+    let (bytes, span) = parse_input(input)?;
+    match CString::new(bytes) {
+        Ok(s) => {
             let mut lit = Literal::byte_string(s.as_bytes_with_nul());
             lit.set_span(span);
-            lit
-        })
-        .map_err(|_| Error::new(span, "nul byte found in the literal"))
-}
-
-struct Input(Vec<u8>, Span);
-
-impl Parse for Input {
-    fn parse<'a>(input: &'a ParseBuffer<'a>) -> Result<Self> {
-        let lookahead = input.lookahead1();
-        if lookahead.peek(LitByteStr) {
-            let b = input.parse::<LitByteStr>().unwrap();
-            return Ok(Input(b.value(), b.span()));
+            Ok(lit)
         }
-        if lookahead.peek(LitStr) {
-            let s = input.parse::<LitStr>().unwrap();
-            return Ok(Input(s.value().into_bytes(), s.span()));
-        }
-        if lookahead.peek(Ident) {
-            let i = input.parse::<Ident>().unwrap();
-            return Ok(Input(i.to_string().into_bytes(), i.span()));
-        }
-        Err(lookahead.error())
+        Err(_) => Err(Error(span, "nul byte found in the literal")),
     }
 }
